ef641c938f393215f5fe3b715be3c31f5975ba2e
sinapourmahmoud <sinapoormahmood@gmail.com>
2023-04-05 16:36:43 +0430

    finish hero part

--- pages/components/Nav.tsx
+++ pages/components/Nav.tsx
@@ -13,13 +13,13 @@ const Nav: React.FC = () => {
         </Link>
         <h3 className="cursor-pointer font-medium">About</h3>
         <h3 className="cursor-pointer font-medium">contact</h3>
-        <h3 className="cursor-pointer font-medium  py-3 px-5 bg-green-400 text-white rounded-3xl">
+        <h3 className="cursor-pointer font-medium  py-3 px-5 bg-green-600 text-white rounded-3xl">
           Server
         </h3>
       </div>
       <div className="md:flex items-center gap-4 hidden">
         <h3 className="cursor-pointer font-medium text-gray-600">contact</h3>
-        <h3 className="cursor-pointer font-medium  py-3 px-5 border border-green-400 text-green-400 rounded-3xl">
+        <h3 className="cursor-pointer font-medium  py-3 px-5 border border-green-600 text-green-600 rounded-3xl">
           Get started
         </h3>
       </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,23 @@ const index = () => {
   return (
     <div className="max-w-7xl mx-auto">
       <Nav />
+      <div className="flex justify-between items-center bg-yellow-400 rounded-md px-2 md:px-5 py-3 lg:py-0">
+        <div className="flex flex-col gap-2">
+          <h2 className="text-6xl font-sarif max-w-xl">
+            <span className="underline underline-offset-4 ">Medium</span> is a
+            place to write, read and connect
+          </h2>
+          <h2>
+            Its easy to read and write comments and bogs for people and have fun
+            and find friend
+          </h2>
+        </div>
+        <img
+          className="hidden h-32 md:block lg:h-full"
+          src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png"
+          alt="medium-logo"
+        />
+      </div>
     </div>
   );
 };

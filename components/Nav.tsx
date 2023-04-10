a3b2d21d4fda1e2f766001722c9a317db298495b
sinapourmahmoud <sinapoormahmood@gmail.com>
2023-04-10 23:48:22 +0430

    doing more functionality for header

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,27 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
+//scroll interface
+
 const Nav: React.FC = () => {
+  let [scrolled, setScrolled] = useState<boolean>(false);
+  const handleScrolling = () => {
+    let pageOffset = window.pageYOffset;
+    if (pageOffset > 30) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", (e: any) => {
+      handleScrolling();
+    });
+    return () => {
+      removeEventListener("scroll", handleScrolling);
+    };
+  }, []);
   return (
-    <header className="flex items-center  px-2 py-4 justify-between">
-      <div className="flex items-center gap-6">
-        <Link href="/">
-          <img
-            className="w-44 cursor-pointer object-contain"
-            src="https://links.papareact.com/yvf"
-            alt="logo"
-          />
-        </Link>
-        <h3 className="cursor-pointer font-medium">About</h3>
-        <h3 className="cursor-pointer font-medium">contact</h3>
-        <h3 className="cursor-pointer font-medium  py-3 px-5 bg-green-600 text-white rounded-3xl">
-          Server
-        </h3>
-      </div>
-      <div className="md:flex items-center gap-4 hidden">
-        <h3 className="cursor-pointer font-medium text-gray-600">contact</h3>
-        <h3 className="cursor-pointer font-medium  py-3 px-5 border border-green-600 text-green-600 rounded-3xl">
-          Get started
-        </h3>
+    <header className={` sticky top-0 bg-white ${scrolled && " shadow-xl"}`}>
+      <div className="mx-auto max-w-7xl flex items-center  px-2 py-4 justify-between">
+        <div className="flex items-center gap-6">
+          <Link href="/">
+            <img
+              className={` cursor-pointer object-contain transition-all duration-200 ${
+                !scrolled ? "w-52" : "w-44"
+              }`}
+              src="https://links.papareact.com/yvf"
+              alt="logo"
+            />
+          </Link>
+          <h3 className="cursor-pointer font-medium">About</h3>
+          <h3 className="cursor-pointer font-medium">contact</h3>
+          <h3 className="cursor-pointer font-medium  py-3 px-5 bg-green-600 text-white rounded-3xl">
+            Server
+          </h3>
+        </div>
+        <div className="md:flex items-center gap-4 hidden">
+          <h3 className="cursor-pointer font-medium text-gray-600">contact</h3>
+          <h3 className="cursor-pointer font-medium  py-3 px-5 border border-green-600 text-green-600 rounded-3xl">
+            Get started
+          </h3>
+        </div>
       </div>
     </header>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -17,32 +17,34 @@ import Head from "next/head";
 const index: React.FC<Props> = ({ posts }) => {
   console.log(posts);
   return (
-    <div className="max-w-7xl mx-auto py-6">
+    <div className=" py-2">
       <Head>
         <title>Blog app</title>
       </Head>
       <Nav />
-      <div className="flex justify-between items-center bg-yellow-400 rounded-sm px-2 md:px-5 py-3 lg:py-0 border-y border-black">
-        <div className="flex flex-col gap-2">
-          <h2 className="text-6xl font-sarif max-w-xl">
-            <span className="underline underline-offset-4 ">Medium</span> is a
-            place to write, read and connect
-          </h2>
-          <h2>
-            Its easy to read and write comments and bogs for people and have fun
-            and find friend
-          </h2>
+      <div className="max-w-7xl mx-auto">
+        <div className="flex justify-between items-center bg-yellow-400 rounded-sm px-2 md:px-5 py-3 lg:py-0 border-y border-black">
+          <div className="flex flex-col gap-2">
+            <h2 className="text-6xl font-sarif max-w-xl">
+              <span className="underline underline-offset-4 ">Medium</span> is a
+              place to write, read and connect
+            </h2>
+            <h2>
+              Its easy to read and write comments and bogs for people and have
+              fun and find friend
+            </h2>
+          </div>
+          <img
+            className="hidden h-32 md:block lg:h-full"
+            src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png"
+            alt="medium-logo"
+          />
         </div>
-        <img
-          className="hidden h-32 md:block lg:h-full"
-          src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png"
-          alt="medium-logo"
-        />
-      </div>
-      <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-3 lg:px-0 mt-4 gap-6">
-        {posts?.map((item) => (
-          <Card item={item} key={item._id} />
-        ))}
+        <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-3 lg:px-0 mt-4 gap-6">
+          {posts?.map((item) => (
+            <Card item={item} key={item._id} />
+          ))}
+        </div>
       </div>
     </div>
   );
